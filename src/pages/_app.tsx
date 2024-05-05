ac43d425251699aa5ca0f8f52cd7ea4a52d71686
Prashant <prashantrahul141@protonmail.com>
2024-05-06 03:18:45 +0530

    add locale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import TimeAgo from "javascript-time-ago";
 import type { AppType } from "next/app";
 import { Inter } from "next/font/google";
 
 import "~/styles/globals.css";
+
+import en from "javascript-time-ago/locale/en";
+
+TimeAgo.addDefaultLocale(en);
 
 const inter = Inter({
   subsets: ["latin"],
