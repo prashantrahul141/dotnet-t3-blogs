47f91556bc7d26c99995240f55a30083eeadca08
Prashant <prashantrahul141@protonmail.com>
2024-05-06 02:03:50 +0530

    update root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,18 @@ import { Inter } from "next/font/google";
 import "~/styles/globals.css";
 
 const inter = Inter({
-	subsets: ["latin"],
-	variable: "--font-sans",
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-	return (
-		<main className={`font-sans ${inter.variable}`}>
-			<Component {...pageProps} />
-		</main>
-	);
+const CisurpRoot: AppType = ({ Component, pageProps }) => {
+  return (
+    <main
+      className={`dark min-h-screen w-full bg-background font-sans antialiased ${inter.variable}`}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
-export default MyApp;
+export default CisurpRoot;
