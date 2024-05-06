10f170370e5828b048a4d1b248e2a0f6ddf85169
Prashant <prashantrahul141@protonmail.com>
2024-05-06 21:26:41 +0530

    fix blogcard to show correct time, remove uncessary imports

--- src/components/Blogs/BlogCard.tsx
+++ src/components/Blogs/BlogCard.tsx
@@ -12,33 +12,22 @@ import { useUserStore } from "../state/stores";
 import { MdModeEditOutline } from "react-icons/md";
 import { Button } from "../ui/button";
 import { useState } from "react";
-
-import { Label } from "@radix-ui/react-label";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/dialog";
-import { Input } from "../ui/input";
 import EditBlogDialog from "./EditBlogDialog";
+import { convertDateUTC } from "~/lib/utils";
 
 const BlogCard = ({ blogData }: { blogData: TBlog }) => {
-  const isAuthor = useUserStore((state) => state.userId) !== blogData.userId;
+  const isAuthor = useUserStore((state) => state.userId) === blogData.userId;
   const [showDialog, setShowDialog] = useState(false);
 
   return (
     <>
-      <Card className="relative">
+      <Card className="group relative">
         <div className="absolute right-0 top-0">
           {isAuthor && (
             <Button
               variant={"ghost"}
               onClick={() => setShowDialog(!showDialog)}
-              className="h-12 w-12 rounded-full"
+              className="h-12 w-12 rounded-full opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100"
             >
               <MdModeEditOutline className="text-primary" />
             </Button>
@@ -53,7 +42,9 @@ const BlogCard = ({ blogData }: { blogData: TBlog }) => {
             >
               @{blogData.userName}
             </Link>{" "}
-            <ReactTimeAgo date={new Date(blogData.createdAt)}></ReactTimeAgo>
+            <ReactTimeAgo
+              date={convertDateUTC(blogData.createdAt)}
+            ></ReactTimeAgo>
           </CardDescription>
         </CardHeader>
         <CardContent>
