47b69062e5e4d7229e7cc96513f76aadda2b1791
Prashant <prashantrahul141@protonmail.com>
2024-05-06 12:50:45 +0530

    mark API_URLS as const

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -2,26 +2,27 @@ export const BASE_API_URL = "http://localhost:3000";
 
 export const API_URLS = {
   Blog: {
-    GetAll: () => `${BASE_API_URL}/api/Blogs/GetAll`,
+    GetAll: () => `${BASE_API_URL}/api/Blogs/GetAll` as const,
     GetBlogByBlogId: (blogId: string) =>
-      `${BASE_API_URL}/api/Blogs/GetBlogByBlogId/${blogId}`,
+      `${BASE_API_URL}/api/Blogs/GetBlogByBlogId/${blogId}` as const,
 
     GetBlogByUserId: (userId: string) =>
-      `${BASE_API_URL}/api/Blogs/GetBlogByUserId/${userId}`,
+      `${BASE_API_URL}/api/Blogs/GetBlogByUserId/${userId}` as const,
 
-    CreateNew: () => `${BASE_API_URL}/api/Blogs/CreateNew`,
+    CreateNew: () => `${BASE_API_URL}/api/Blogs/CreateNew` as const,
     UpdateExisting: (blogId: number) =>
-      `${BASE_API_URL}/api/Blogs/UpdateExisting/${blogId}`,
+      `${BASE_API_URL}/api/Blogs/UpdateExisting/${blogId}` as const,
   },
 
   User: {
-    GetLoggedInUser: () => `${BASE_API_URL}/api/User/GetLoggedInUser`,
-    GetLoggedInUserImage: () => `${BASE_API_URL}/api/User/GetLoggedInUserImage`,
+    GetLoggedInUser: () => `${BASE_API_URL}/api/User/GetLoggedInUser` as const,
+    GetLoggedInUserImage: () =>
+      `${BASE_API_URL}/api/User/GetLoggedInUserImage` as const,
   },
 
   Auth: {
-    Register: () => `${BASE_API_URL}/register`,
-    Login: () => `${BASE_API_URL}/login`,
-    Refresh: () => `${BASE_API_URL}/refresh`,
+    Register: () => `${BASE_API_URL}/register` as const,
+    Login: () => `${BASE_API_URL}/login` as const,
+    Refresh: () => `${BASE_API_URL}/refresh` as const,
   },
 };
