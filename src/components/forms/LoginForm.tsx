bc8eee52f60419d48e82b90b3cab1b0e77e71e81
Prashant <prashantrahul141@protonmail.com>
2024-05-06 14:17:49 +0530

    actually login user

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,7 +1,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
+import useSWRMutation from "swr/mutation";
 import { z } from "zod";
-
 import { Button } from "~/components/ui/button";
 import {
   Form,
@@ -12,25 +13,73 @@ import {
   FormMessage,
 } from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
+import { API_URLS } from "~/lib/consts";
+import { useToast } from "../ui/use-toast";
+
+const responseSchema = z.object({
+  accessToken: z.string(),
+  expiresIn: z.number(),
+  refreshToken: z.string(),
+});
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+  email: z.string().email(),
   password: z.string().min(2).max(18),
 });
 
+async function loginUser(
+  url: string,
+  { arg }: { arg: z.infer<typeof formSchema> },
+) {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(arg),
+  });
+
+  if (res.status === 200) {
+    const resJson = await res.json();
+    const parsed = await responseSchema.safeParseAsync(resJson);
+    if (parsed.success) {
+      sessionStorage.setItem("accessToken", parsed.data.accessToken);
+      sessionStorage.setItem("refreshToken", parsed.data.refreshToken);
+    }
+  }
+
+  return res.status === 200;
+}
+
 const LoginForm = () => {
+  const router = useRouter();
+  const { toast } = useToast();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      email: "",
       password: "",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const { trigger, isMutating } = useSWRMutation(
+    API_URLS.Auth.Login(),
+    loginUser,
+    {},
+  );
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const success = await trigger(values);
+    if (success) {
+      router.push("/");
+    } else {
+      toast({
+        title: "Unable to login",
+        description: "Email or password did not match.",
+      });
+    }
   };
 
   return (
@@ -38,12 +87,12 @@ const LoginForm = () => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="email"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Email</FormLabel>
               <FormControl>
-                <Input placeholder="Cisurp" {...field} />
+                <Input placeholder="Cisurp@email.com" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -63,8 +112,8 @@ const LoginForm = () => {
             </FormItem>
           )}
         />
-        <Button type="submit" className="rounded-lg">
-          Submit
+        <Button disabled={isMutating} type="submit" className="rounded-lg">
+          Login
         </Button>
       </form>
     </Form>
