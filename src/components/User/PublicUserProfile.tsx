cf5e06d9c7c7cee3930d92381bd713a7fbbef26f
Prashant <prashantrahul141@protonmail.com>
2024-05-08 10:44:38 +0530

    add public user profile page

---
+++ src/components/User/PublicUserProfile.tsx
@@ -0,0 +1,33 @@
+import { TPublicUser } from "~/types";
+import BlogListUser from "../Blogs/BlogListUser";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+
+const PublicUserProfile = ({ user }: { user: TPublicUser }) => {
+  return (
+    <>
+      <Card className="mx-auto mb-12 flex w-full max-w-xl">
+        <CardHeader>
+          <CardTitle className="flex flex-col items-center gap-7 md:flex-row">
+            <Avatar>
+              <AvatarImage
+                src={user.avatar}
+                className="h-44 w-44 rounded-full"
+              />
+              <AvatarFallback>PF</AvatarFallback>
+            </Avatar>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="mt-12">
+            <p className="text-sm text-muted-foreground">Username</p>
+            <p className="text-lg font-bold">{user.username}</p>
+          </div>
+        </CardContent>
+      </Card>
+      <BlogListUser userId={user.userId}></BlogListUser>
+    </>
+  );
+};
+
+export default PublicUserProfile;
